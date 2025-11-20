c9944ee4e8a3843d4d7e3e4ed27ba6fc8855af82
krish <rokrish@calpoly.edu>
2025-11-19 22:08:56 -0800

    Framework

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+class WaterUsage:
+
+def __init__(self, shower_time: int, flushes: int, sink_time: int, housing: str):
+    self.shower_time = shower_time
+    self.flushes = flushes
+    self.sink_time = sink_time
+    self.housing = housing
+
+
+def shower_time_to_gallons(time: list[WaterUsage]):
+
+def flushes_to_gallons(flushes: list[WaterUsage]):
+
+def sink_time_to_gallons(time: list[WaterUsage]):
+
+def average_water_usage(usage: list[WaterUsage], housing: str) -> float:
+    total_gallons = 0
+
