95987ff3e5a51d1867e94a865049713bde6d41c0
krish <rokrish@calpoly.edu>
2025-11-24 23:45:51 -0800

    Fixed formatting on repr statement.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class WaterUsage:
         self.housing = housing
 
     def __repr__(self):
-        return f"{self.shower_time}, {self.flushes}, {self.sink_time}, {self.housing}"
+        return "Your shower usage is {} minutes, your sink usage is {} minutes, your toilet flushes number {}, and you live in {} housing.".format(self.shower_time, self.sink_time, self.flushes, self.housing)
 
 def shower_time_to_gallons(time: list[WaterUsage]):
 
